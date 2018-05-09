539cc28bc0e42950bde175d44759e6d1b7e6f4e5
BSIofKOR <z0564@naver.com>
2018-05-09 09:18:57 +0900

    c

---
+++ R/hello.R
@@ -0,0 +1,32 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+# ctrl + Shift + L = 로딩
+
+hello <- function() {
+  print("Hello, world!!!")
+}
+
+hello2 <- function() {
+  print("Hello2, world!")
+}
+
+hello()
+
+library()
+
+devtools::install_github("ksublee/mhawkes")
+.git
+file.exists("~/.ssh/id_rsa.pub")
